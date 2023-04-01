40c123ca3f7c415c85679906f9df623e069afef6
Travis Gaff <travis@labzero.com>
2023-04-01 09:16:37 +0900

    some initial tailwind setup / test & background

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
+    backgroundImage: {
+      'hero-pattern': "url(\"data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 16 16' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 0h16v2h-6v6h6v8H8v-6H2v6H0V0zm4 4h2v2H4V4zm8 8h2v2h-2v-2zm-8 0h2v2H4v-2zm8-8h2v2h-2V4z' fill='%236625cf' fill-opacity='0.24' fill-rule='evenodd'/%3E%3C/svg%3E\")"
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),

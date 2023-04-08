a53ca98a2485bda53431e6f021b92a73f9eebb87
Travis Gaff <travis@labzero.com>
2023-04-08 09:14:46 +0900

    example of intercepting and altering turbo-stream

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,14 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+// addEventListener("turbo:before-stream-render", ((event) => {
+//   const fallbackToDefaultActions = event.detail.render
+
+//   event.detail.render = function (streamElement) {
+//     /* optional other logic or processing before
+//        conditionally handing off to default render
+//     */
+//     fallbackToDefaultActions(streamElement)
+//   }
+// }))

b19b1f6ebebe788a598aa68357d6f315e756ee58
Travis Gaff <travis@labzero.com>
2023-04-10 16:23:10 +0900

    add user model and current be rails g model User name:string password_digest:string

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true, uniqueness: true, length: { min: 3 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_04_073058) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_071846) do
   create_table "messages", force: :cascade do |t|
     t.integer "support_chat_id", null: false
     t.string "text", null: false
@@ -23,6 +23,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_073058) do
   create_table "support_chats", force: :cascade do |t|
     t.string "user_id"
     t.string "user_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

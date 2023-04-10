c84f6cefa809a4f9b2b9eb41ea217dea80296b61
Travis Gaff <travis@labzero.com>
2023-04-10 13:12:03 +0900

    add pry for dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  console do
+    # this block is called only when running console,
+    # so we can safely require pry here
+    require "pry"
+    config.console = Pry
+  end
 end

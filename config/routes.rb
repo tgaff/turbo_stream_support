047db5c35134205e51df690590ffd9cd04ebab89
Travis Gaff <travis@labzero.com>
2023-04-10 17:14:15 +0900

    partial login form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :support_chats, only: [ :create, :show, :index, :new ] do
     resources :messages, only: [ :create, :show ]
   end
+  resources :sessions, except: [ :update, :show, :index, :edit ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

086bb61c25a229d31d00bf949821897e2b7fa96c
Travis Gaff <travis@labzero.com>
2023-04-19 13:59:45 +0900

    add some users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+['Tina', 'Teddy'].each do |name|
+  u = User.find_or_initialize_by(name: name)
+  u.password = 'password'
+  u.save!
+end

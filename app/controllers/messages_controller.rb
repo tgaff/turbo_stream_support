0724eb484201dc9e4c18cf990715cdfe97b3d61b
Travis Gaff <travis@labzero.com>
2023-05-15 21:29:27 +0900

    submit new messages and display

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,20 @@
+class MessagesController < ApplicationController
+  before_action :set_support_chat
+
+  def create
+    @message = @support_chat.messages.create(message_params)
+    respond_to do |format|
+      format.html { redirect_to @support_chat }
+    end
+  end
+
+  private
+
+  def message_params
+    params.require('message').permit('text')
+  end
+
+  def set_support_chat
+    @support_chat = SupportChat.find(params[:support_chat_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :support_chats
+  resources :support_chats do
+    resources :messages, only: [:create]
+  end
   get 'static/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

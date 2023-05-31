da88810bb147201ce4cb225f3a1353f5952bfcc7
Travis Gaff <travis@labzero.com>
2023-05-31 14:31:35 +0900

    many records

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-sc = SupportChat.first
-
 messages = [
   { text: "Hello, I'm having trouble with my blaster.", side: :right },
   { text: "I'm sorry to hear that. What seems to be the problem?", side: :left },
@@ -18,7 +15,7 @@ messages = [
   { text: "Have you tried replacing the power cell?", side: :left },
   { text: "Yes, I've tried that too.", side: :right },
   { text: "Didn't help.", side: :right },
-  { text: "I see. Well, I'm not sure what else to do. Maybe you should try contacting your local Imperial armory.  You might be able to have it replaced.", side: :left },
+  { text: "I see. Well, I'm not sure what else to do. Maybe you should try contacting your local Imperial armory.  You might be able to have it replaced.", side: :left},
   { text: "But I'm on a mission!", side: :right },
   { text: "I understand, but I'm not a stormtrooper. I'm just a technical support droid.", side: :left },
   { text: "Fine.", side: :right },
@@ -29,6 +26,15 @@ messages = [
   { text: "You too.", side: :right }
 ]
 
-messages.each do |msg|
-  Message.create(text: msg[:text], support_chat: sc)
+SupportChat.all.each do |sc|
+  # next if sc.messages.count > 10
+
+  100.times do
+    messages.each do |m|
+      sc.messages.create(text: m[:text])
+    end
+  end
+
+  sc.reload
+  puts "SC with ID #{sc.id} has #{sc.messages.count} messages"
 end

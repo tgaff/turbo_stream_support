1731f51445b6a9081fecc1fb26242d62e8fe7500
Travis Gaff <travis@labzero.com>
2023-06-07 19:29:03 +0900

    add author

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_10_074811) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_102802) do
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "messages", force: :cascade do |t|
     t.integer "support_chat_id", null: false
     t.string "text"
